631907cd2d3f80e48d53ba3e34c05e8cb20dca27
SEIFU MEKONNEN TOLOSA <tms.0xvoid@gmail.com>
2022-09-14 19:48:53 +0100

    example files

---
+++ example/demofile copy.js
@@ -0,0 +1,65 @@
+export const data = [
+  {
+    id: 'op',
+    text: `In 2018 I moved 200 miles away from my partner for work after we graduated from college. I would drive to see him every other weekend. He has not had stable employment at all in the four years since we graduated. He did not drive to see me once in the four years I lived in my apartment.
+
+    At the start of the pandemic, I drove to his apartment. I didn’t know when we were going to be called back to the office, so I kept my apartment thinking things might calm down in a few weeks. I ended up staying with him for a few months leaving only because I needed to check on my apartment.
+    
+    Throughout the pandemic I frequently stayed with him for extended amounts of time. I kept my apartment because my job never made a call on going fully remote and because my partner and I fight sometimes and I sometimes had to leave because he wanted me to. If he were reading this he would insist I mention that I did not always leave when we fought, so I will add that if the weather was bad or if it was late, he would let me stay the night then leave the following day if we were still having problems.
+    
+    Sometimes when we would fight he would comment about me living with him “rent free,” and I would argue that I did not live with him. I had my own place and was living out of a suitcase to visit him for extended periods of time because it was the only way we would see each other since he never visited me. This was the only time we talked about rent.
+    
+    Because my partner has almost no work experience, he will be making significantly less than I do in the job he starts tomorrow (assuming he takes it as he feels it’s not worth his time). He now feels that I owe him half the rent and utilities he paid over the last two years because he was “supporting me.” I just gave up my apartment and he feels I should also pay most of the rent going forward because I make more than him.
+    
+    Despite making almost $0 throughout the pandemic, my partner had two hair transplants, lived in a luxury apartment complex in the most expensive city in our state, and bought a new car in the same time frame. He paid for this with a combination of money received from his parents and a settlement from a car accident in his teens. And I think he genuinely feels that he is more deserving of my money than I am.
+    
+    Tl;dr: I stayed with my boyfriend throughout most of the pandemic, but kept my apartment. My boyfriend is now saying I owe him two years of rent.
+    
+    EDIT: He just apologized and said he was just having a bad day. This is the second time he’s mentioned this though. I’m not sure how serious he really is about it, but it seems like he may just be saying it because he feels bad about where he is in life.
+    
+    Thanks everyone!`,
+  },
+  {
+    id: 'grockit1',
+    text: `I saw your update. Do but think for a sucker second that if you had actually given him the ridiculous demands for rent money that he would have turned it down saying he had just had a bad day.
+
+    He was just seeing what he could get away with and when he realized you were going to push back, he backed down and pretended like he didn't mean it.
+    
+    TBF, he sounds like a fucking asshole. What do you even see in him? It's not healthy to be fighting in relationships all the time like that. You can do much better, I promise.`,
+  },
+  {
+    id: 'semcg',
+    text: `Thats pretty manipulative of him to drop this on you after you got rid of your place.
+
+    It sounds like he doesnt want to work so hes going to use you as a way to pay for that now that a job has been offered to him.`,
+  },
+  { id: 'bigass', text: 'Got a point … men suck' },
+  {
+    id: 'nhavar',
+    text: `Nope yourself right out of this relationship. There's nothing that you've said that makes this sound like a good relationship or one that will get better. He sounds like an entitled child versus a functional adult. Break it off now and do not entertain any crap he gives you about what you owe him. You owe him nothing.`,
+  },
+  {
+    id: 'lndnpenni',
+    text: `I agree. He will keep raising this at times convenient to him.`,
+  },
+  {
+    id: 'rickelzy',
+    text: `You're putting far more effort into this relationship than he is. You're the one with a steady job and he's living outside his means and expects you to foot the bill on everything. He may be apologizing now because he realized he's poked the bear too hard demanding that much money, but I suspect he very much will still see you as a meal ticket helping to pay his bills going forward. Make it clear to him that you will not be giving him any money and will not be moving in with him for the foreseeable future, and he can make more effort to visit you if he wants to see you. His reaction should tell you all you need to know, but after (over?) 4 years it sounds like this is who he is and what you can expect from him.`,
+  },
+  {
+    id: '_Butterflyneedle_',
+    text: `Normal partner: "hey, I love that you're coming over here to see me and the time we can spend together because of it, but the utilities bills have gone up a lot so I'm just wondering if you pitch in a couple of tenners?"
+
+    And then there's your manipulative and backhanded BF.`,
+  },
+  {
+    id: '_jamesbaxter',
+    text: `No, no, no. Do not pay him anything.
+
+    If there is no firm agreement in place beforehand - preferably in writing - nothing is owed and you need to stick to this.
+    
+    If you pay him, who knows what else he will later try to charge you for after the fact. I believe if this becomes a pattern there are legal implications as well, he would have reasonable expectation that you would pay him for whatever he demands.
+    
+    Honestly it sounds like he’s using you and/or halfway out the door. He could be trying to make sure you pay up before he dumps you.`,
+  },
+]
---
+++ example/demofile_1.js
@@ -0,0 +1,1 @@
+export const data = `In 2018 I moved 200 miles away from my partner for work after we graduated from college. I would drive to see him every other weekend.`
